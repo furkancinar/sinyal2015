f92a4b6cbc9b59ea77ca11da02e9d29ddaef1736
Furkan Çınar <cinar.furkan@outlook.com>
2015-12-15 12:52:26 +0200

    istenilen

--- frek.m
+++ frek.m
@@ -1,19 +1,19 @@
 function ff=frek(nota,ok)
 
-notalar=['A' 'a' 'B' 'b' 'C' 'c' 'D' 'd' 'E' 'e' 'F' 'f' 'G' 'g'];
+notalar=['C' 'c' 'D' 'd' 'E' 'F' 'f' 'G' 'g' 'A' 'a' 'B']; %notalarýn karþýlýðý olarak tanýmlanan dizi deðerleri%
 
-sabitdeger=16.35;
+sabitdeger=16.35; %seçilen sabit deðer%
 
 p=length(notalar);
 
 
 
-for okt= 0:8
-    if okt==ok
+for okt= 0:8 %döngünün oktav sayýsý kadar dönmesi için bu deðerler seçildi%
+    if okt==ok % dögüdeli oktav ile istenilen oktavýn eþitliðinin kotrol edildiði kýsým%
         
 for j=1:p
-    if nota==char(notalar(j)) 
-        ff=2^okt*(2^((j-1)/12)*sabitdeger);
+    if nota==char(notalar(j)) %istenilen nota ve dizideki notalarýn birbirine eþitliðini kontrol eden kýsým%
+        ff=2^okt*(2^((j-1)/12)*sabitdeger); %istenilen notanýn frekans karþýlýðýný bulan formül%
     end
 end
 

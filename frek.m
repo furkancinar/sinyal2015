c1c7316df3cd3abca8d90197952ab7239c3f78c3
Furkan Çınar <cinar.furkan@outlook.com>
2015-12-15 12:31:00 +0200

    yazılan notanın frekans karşılığını getiren fonksiyon yazıldı.

--- frek.m
+++ frek.m
@@ -1,17 +1,23 @@
-function ff=frek(nota,oktav)
-notalar=['A' 'a' 'B' 'b'];
-referans=16.35;
+function ff=frek(nota,ok)
+
+notalar=['A' 'a' 'B' 'b' 'C' 'c' 'D' 'd' 'E' 'e' 'F' 'f' 'G' 'g'];
+
+sabitdeger=16.35;
+
 p=length(notalar);
 
-for okt=0:8
-    if okt==oktav
+
+
+for okt= 0:8
+    if okt==ok
         
 for j=1:p
     if nota==char(notalar(j)) 
-        ff=2^okt*(2^((j-1)/12)*referans);
+        ff=2^okt*(2^((j-1)/12)*sabitdeger);
     end
 end
 
     end
 end
+
 end

ccbb26dc99fcb7d03155d25536288d80a2e665e6
Furkan Çınar <cinar.furkan@outlook.com>
2016-01-05 13:03:48 +0200

    sentez.m dosyası içine notalar.txt dosyasını okuyan kodlar yazıldı.

--- sentez.m
+++ sentez.m
@@ -1,26 +1,54 @@
-[nota,b,c]=textread('notalar.txt','%s %s %s'); 
-fs=8192;
-ds=round(fs/100); 
-go=round(fs/10); 
-notalar=zeros(1,fs);
-a=zeros(1,length(notalar)); 
-durak=zeros(1,ds);
-oktav=str2num(cell2mat(b)); 
-   for i=1:(length(nota)) 
-       vurus((i),1)=str2num(cell2mat(c(i,1))); 
+
+%% Matris tanýmlandý ve dosya okuma iþlemi yapýldý.
+[nota,k,t]=textread('notalar.txt','%s %s %s'); 
+%%  Frekans deðeri tanýmlandý
+fs=10000;
+
+%% Duraklama deðeri
+ds=round(fs/100);
+
+%% Gecikme deðeri
+gecikme=round(fs/10); 
+
+%% 
+notalar=zeros(1,fs); % Zeros=0 matris
+a=zeros(1,length(notalar));% notalarýn karakterlerini sayýp  a deðiþkenine atýyor. 
+durak=zeros(1,ds); 
+oktav=str2num(cell2mat(k)); %str2num=karakter olan bir deðeri sayý deðerine atar.
+
+%%
+  
+for i=1:(length(nota)) %döngünün baþlangýç deðeri 1 den baþlýyor ve notalarýn bitimine kadar deva ediyor. 
+       vurus((i),1)=str2num(cell2mat(t(i,1))); % 
+       
        frekans((i),1)=frek(nota((i),1),oktav((i),1)); 
-       [x,t]=note(frekans((i),1),vurus((i),1));
+       
+       [x,z]=note(frekans((i),1),vurus((i),1));
+       
        notalar=[notalar durak x]; 
-   end 
+end 
+ 
+
  notalarecho=zeros(1,(length(notalar))); 
+ 
+ %Echo oluþturma%
  for i=1:(length(notalar)) 
-     f=(go+i); 
+     
+     f=(gecikme+i); 
      if f<(length(notalar)) 
      notalarecho(i)=notalar(i)*0.3+notalar(f); 
      else 
       notalarecho(i)=notalar(i)*0.3+0; 
      end
  end 
+ 
+ %% 
   normalizenotalarecho=zeros(1,(length(notalar)));
-  normalizenotalarecho=notalarecho/max(abs(notalarecho));
-plot(normalizenotalarecho) 
+  
+  %% Normalize etme iþlemini yapýyor
+  normalizenotalarecho=notalarecho/max(abs(notalarecho)); 
+  
+  %% Sesli olarak dinlemek için
+  sound(normalizenotalarecho)
+  %% Sinyali çizdirmek için
+  plot(normalizenotalarecho)
